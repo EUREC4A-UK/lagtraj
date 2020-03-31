a2a9a369697bcad463eb2336f16cdbcdede081e9
Leif Denby <leif@denby.eu>
2020-03-31 17:36:24 +0100

    create tests for calling routines directly

--- tests/test_forcing_profiles_extraction.py
+++ tests/test_forcing_profiles_extraction.py
@@ -2,11 +2,35 @@ from pathlib import Path
 import dateutil.parser
 import datetime
 
+import xarray as xr
+import numpy as np
+
 import pytraj.produce.lagrangian_trajectory
 import pytraj.produce.forcing_profiles
 
 
-def test_create_stationary_trajectory():
+def test_create_dummy_forcing_from_stationary_trajectory():
+    ds_traj = pytraj.produce.lagrangian_trajectory.stationary.extract_trajectory(
+        lat0=-10, lon0=40,
+        t0=dateutil.parser.parse("2020-01-22T12:00"),
+        t_max=dateutil.parser.parse("2020-01-24T12:00"),
+        dt=datetime.timedelta(hours=4),
+    )
+
+    da_levels = xr.DataArray(
+        np.array([100e3, 90e3, 80e3, 70e3]),
+        attrs=dict(long_name='pressure levels', units='Pa')
+    )
+
+    ds_forcing_profiles = pytraj.produce.forcing_profiles.dummy.extract_forcing_profiles(
+        ds_traj=ds_traj,
+        required_variables=["ddt__qv",],
+        da_levels=da_levels
+    )
+
+
+# testing command line interface
+def test_main():
     fn_trajectory = "trajectory_test.nc"
     pytraj.produce.lagrangian_trajectory.main(
         lat0=-10, lon0=40,
--- tests/test_trajectory_creation.py
+++ tests/test_trajectory_creation.py
@@ -3,9 +3,21 @@ import dateutil.parser
 import datetime
 
 import pytraj.produce.lagrangian_trajectory
+from pytraj.utils import validation
 
 
 def test_create_stationary_trajectory():
+    ds = pytraj.produce.lagrangian_trajectory.stationary.extract_trajectory(
+        lat0=-10, lon0=40,
+        t0=dateutil.parser.parse("2020-01-22T12:00"),
+        t_max=dateutil.parser.parse("2020-01-24T12:00"),
+        dt=datetime.timedelta(hours=4),
+    )
+    validation.validate_trajectory(ds)
+
+
+# testing command line interface
+def test_main():
     fn_out = "stationary_trajectory_test.nc"
     pytraj.produce.lagrangian_trajectory.main(
         lat0=-10, lon0=40,

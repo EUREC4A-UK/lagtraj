b868f4a0d9d1acf64b80dcc80a0a0b5027827aa2
Leif Denby <leif@denby.eu>
2020-03-31 16:53:26 +0100

    remove timezone info because xarray can't handle it right now

--- tests/test_trajectory_creation.py
+++ tests/test_trajectory_creation.py
@@ -9,8 +9,8 @@ def test_create_stationary_trajectory():
     fn_out = "stationary_trajectory_test.nc"
     pytraj.produce.lagragian_trajectory.main(
         lat0=-10, lon0=40,
-        t0=dateutil.parser.parse("2020-01-22T12:00Z"),
-        t_max=dateutil.parser.parse("2020-01-24T12:00Z"),
+        t0=dateutil.parser.parse("2020-01-22T12:00"),
+        t_max=dateutil.parser.parse("2020-01-24T12:00"),
         dt=datetime.timedelta(hours=4),
         trajectory_type='stationary',
         out_filename=fn_out,

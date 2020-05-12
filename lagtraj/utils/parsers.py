d6e2803176e94452cbde15dc379c6cee559228e2
Steven Boeing <stevenboeing@localhost.localdomain>
2020-05-12 06:55:40 +0100

    Additional parsers for trajectory and forcing filenames, different filename notation

--- lagtraj/utils/parsers.py
+++ lagtraj/utils/parsers.py
@@ -1,3 +1,11 @@
 def domain_filename_parse(date,prefix_str,directories_dict,domain_dict):
-    filename=directories_dict['output_domains_dir']+'/ml_'+date+'_'+str(domain_dict['lat_max'])+'_'+str(domain_dict['lon_min'])+'_'+str(domain_dict['lat_min'])+'_'+str(domain_dict['lon_max'])+'.nc'
+    filename=directories_dict['output_domains_dir']+'/'+prefix_str+'_'+date+'_'+domain_dict['name']+'.nc'
+    return filename
+
+def trajectory_filename_parse(directories_dict,trajectory_dict):
+    filename=directories_dict['output_trajectories_dir']+'/traj_'+trajectory_dict['name']+'.nc'
+    return filename
+    
+def forcing_filename_parse(directories_dict,forcing_dict):
+    filename=directories_dict['output_forcings_dir']+'/for_'+forcing_dict['name']+'.nc'
     return filename

ed92d986b6eeae3618b642a880e02ce53b74e29f
Steven Boeing <stevenboeing@localhost.localdomain>
2020-05-13 10:55:56 +0100

    Parse filename from dictionary or string

--- lagtraj/utils/parsers.py
+++ lagtraj/utils/parsers.py
@@ -1,11 +1,20 @@
-def domain_filename_parse(date,prefix_str,directories_dict,domain_dict):
-    filename=directories_dict['output_domains_dir']+'/'+prefix_str+'_'+date+'_'+domain_dict['name']+'.nc'
+def domain_filename_parse(date,prefix_str,directories_dict,domain_input):
+    if type(domain_input) == dict:
+        filename=directories_dict['output_domains_dir']+'/'+prefix_str+'_'+date+'_'+domain_input['name']+'.nc'
+    if type(domain_input) == str:
+        filename=directories_dict['output_domains_dir']+'/'+prefix_str+'_'+date+'_'+domain_input['name']+'.nc'
     return filename
 
-def trajectory_filename_parse(directories_dict,trajectory_dict):
-    filename=directories_dict['output_trajectories_dir']+'/traj_'+trajectory_dict['name']+'.nc'
+def trajectory_filename_parse(directories_dict,trajectory_input):
+    if type(trajectory_input) == dict:
+        filename=directories_dict['output_trajectories_dir']+'/traj_'+trajectory_input['name']+'.nc'
+    if type(trajectory_input) == str:
+        filename=directories_dict['output_trajectories_dir']+'/traj_'+trajectory_input+'.nc'
     return filename
     
-def forcings_filename_parse(directories_dict,forcings_dict):
-    filename=directories_dict['output_forcings_dir']+'/for_'+forcings_dict['name']+'.nc'
+def forcings_filename_parse(directories_dict,forcings_input):
+    if type(forcings_input) == dict:  
+        filename=directories_dict['output_forcings_dir']+'/for_'+forcings_input['name']+'.nc'
+    if type(trajectory_input) == str:
+        filename=directories_dict['output_forcings_dir']+'/for_'+forcings_input+'.nc'
     return filename

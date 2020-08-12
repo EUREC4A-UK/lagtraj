21d28f4d9452e5d1d70546c03b7c2090ac7a50f2
Steven Boeing <stevenboeing@localhost.localdomain>
2020-08-12 10:17:22 +0100

    Gradient calculation: comments and use of numba

--- lagtraj/utils/gradient_calculation.py
+++ lagtraj/utils/gradient_calculation.py
@@ -19,6 +19,9 @@ def _boundary_gradients(x_array, y_array, val_array):
     dval_dy = np.empty((len_lons))
     for this_time in range(len_temp):
         for this_level in range(len_levels):
+            # This calculates the x-gradient at each latitude
+            # The ~np.isnan operation is used to filter out masked values
+            # The first and last filtered values are used
             for this_lat in range(len_lats):
                 vals_at_lat = val_array[this_time, this_level, this_lat, :].flatten()
                 x_at_lat = x_array[this_lat, :].flatten()
@@ -26,6 +29,7 @@ def _boundary_gradients(x_array, y_array, val_array):
                 x_filtered = x_at_lat[~np.isnan(vals_at_lat)]
                 dvals = vals_filtered[-1] - vals_filtered[0]
                 dval_dx[this_lat] = dvals / (x_filtered[-1] - x_filtered[0])
+            # This similarly calculates the y-gradient at each longitude
             for this_lon in range(len_lons):
                 vals_at_lon = val_array[this_time, this_level, :, this_lon].flatten()
                 y_at_lat = y_array[:, this_lon].flatten()
@@ -33,12 +37,46 @@ def _boundary_gradients(x_array, y_array, val_array):
                 y_filtered = y_at_lat[~np.isnan(vals_at_lon)]
                 dvals = vals_filtered[-1] - vals_filtered[0]
                 dval_dy[this_lon] = dvals / (y_filtered[-1] - y_filtered[0])
+            # Average these gradients (not weighted at this point, but filtering out all nan values due to e.g. division by zero!)
             x_gradient_array[this_time, this_level] = np.mean(
                 dval_dx[~np.isnan(dval_dx)]
             )
             y_gradient_array[this_time, this_level] = np.mean(
                 dval_dy[~np.isnan(dval_dy)]
             )
+    return x_gradient_array, y_gradient_array
+
+
+@njit
+def _regression_gradients(x_array, y_array, val_array):
+    """Numba function for regression gradients"""
+    len_temp = np.shape(val_array)[0]
+    len_levels = np.shape(val_array)[1]
+    x_gradient_array = np.empty((len_temp, len_levels))
+    y_gradient_array = np.empty((len_temp, len_levels))
+    x_flat = x_array.flatten()
+    y_flat = y_array.flatten()
+    ones_flat = np.ones(np.shape(x_flat))
+    for this_time in range(len_temp):
+        for this_level in range(len_levels):
+            # For each level and time, put all valid (non-nan) x and y locations
+            # as well as the corresponding data, in 1D arrays
+            data_flat = val_array[this_time, this_level, :, :].flatten()
+            data_flat_filter = np.expand_dims(data_flat[~np.isnan(data_flat)], axis=1)
+            x_flat_filter = np.expand_dims(x_flat[~np.isnan(data_flat)], axis=1)
+            y_flat_filter = np.expand_dims(y_flat[~np.isnan(data_flat)], axis=1)
+            ones_flat_filter = np.expand_dims(ones_flat[~np.isnan(data_flat)], axis=1)
+            oxy_mat = np.hstack((ones_flat_filter, x_flat_filter, y_flat_filter))
+            # Use the normal method to find the best fit
+            theta = np.dot(
+                np.dot(
+                    np.linalg.pinv(np.dot(oxy_mat.transpose(), oxy_mat)),
+                    oxy_mat.transpose(),
+                ),
+                data_flat_filter,
+            )
+            x_gradient_array[this_time, this_level] = theta[0][1]
+            y_gradient_array[this_time, this_level] = theta[0][2]
     return x_gradient_array, y_gradient_array
 
 
@@ -63,31 +101,8 @@ def _era5_regression_gradients(da_box, ds_ref_pt):
     # Use the center latitude for projection onto plane
     x_array = lon_dist(lon1_point, lon2_mg, lat1_point)
     y_array = lat_dist(lat1_point, lat2_mg)
-    x_flat = x_array.flatten()
-    y_flat = y_array.flatten()
-    ones_flat = np.ones(np.shape(x_flat))
-    len_temp = np.shape(da_box)[0]
-    len_levels = np.shape(da_box)[1]
-    x_gradient_array = np.empty((len_temp, len_levels))
-    y_gradient_array = np.empty((len_temp, len_levels))
-    for this_time in range(len_temp):
-        for this_level in range(len_levels):
-            data_flat = da_box[this_time, this_level, :, :].values.flatten()
-            data_flat_filter = data_flat[~np.isnan(data_flat)][:, None]
-            x_flat_filter = x_flat[~np.isnan(data_flat)][:, None]
-            y_flat_filter = y_flat[~np.isnan(data_flat)][:, None]
-            ones_flat_filter = ones_flat[~np.isnan(data_flat)][:, None]
-            oxy_mat = np.hstack((ones_flat_filter, x_flat_filter, y_flat_filter))
-            theta = np.dot(
-                np.dot(
-                    np.linalg.pinv(np.dot(oxy_mat.transpose(), oxy_mat)),
-                    oxy_mat.transpose(),
-                ),
-                data_flat_filter,
-            )
-            x_gradient_array[this_time, this_level] = theta[1]
-            y_gradient_array[this_time, this_level] = theta[2]
-    return x_gradient_array, y_gradient_array
+    val_array = da_box.values
+    return _regression_gradients(x_array, y_array, val_array)
 
 
 def calc_horizontal_gradients(da_field, ds_ref_pt, method="regression"):

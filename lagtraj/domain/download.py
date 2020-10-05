88379f9ba0ef653c6fa66fc8c1e87e4a3b555732
Leif Denby <leif@denby.eu>
2020-10-05 16:41:46 +0100

    Add retry rate for downloading domain data (#58)
    * Add retry rate for downloading domain data
    
    Adds a flag to the CLI `--retry-rate` which causes the download utility to sleep (for the number of minutes given) before retrying download of any files not yet completed

--- lagtraj/domain/download.py
+++ lagtraj/domain/download.py
@@ -1,10 +1,12 @@
 import dateutil.parser
 from pathlib import Path
+from time import sleep
 
 from .sources import era5
 from .. import DEFAULT_ROOT_DATA_PATH
 from . import LatLonBoundingBox, LatLonSamplingResolution, build_domain_data_path
 from .load import load_definition
+from ..trajectory.load import load_definition as load_traj_definition
 
 
 def download(
@@ -77,25 +79,68 @@ def download_complete(root_data_path, domain_name):
         )
 
 
-if __name__ == "__main__":
+def _run_cli(timedomain_lookup="by_arguments"):
     import argparse
 
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("domain")
-    argparser.add_argument("start_date", type=dateutil.parser.parse)
-    argparser.add_argument("end_date", type=dateutil.parser.parse)
+    if timedomain_lookup == "by_arguments":
+        argparser.add_argument("domain")
+        argparser.add_argument("start_date", type=dateutil.parser.parse)
+        argparser.add_argument("end_date", type=dateutil.parser.parse)
+    elif timedomain_lookup == "by_trajectory":
+        argparser.add_argument("trajectory")
+    else:
+        raise NotImplementedError(timedomain_lookup)
+
     argparser.add_argument(
         "-d", "--data-path", default=DEFAULT_ROOT_DATA_PATH, type=Path
     )
     argparser.add_argument(
         "-o", "--overwrite", dest="l_overwrite", action="store_true", default=False
     )
+    argparser.add_argument(
+        "--retry-rate",
+        default=None,
+        type=float,
+        help="Retry time delay (in minutes) when some files are still processing",
+    )
     args = argparser.parse_args()
 
-    download_named_domain(
-        data_path=args.data_path,
-        name=args.domain,
-        start_date=args.start_date,
-        end_date=args.end_date,
-        overwrite_existing=args.l_overwrite,
-    )
+    if timedomain_lookup == "by_arguments":
+        domain = args.domain
+        t_min = args.start_date
+        t_max = args.end_date
+    elif timedomain_lookup == "by_trajectory":
+        traj_defn = load_traj_definition(
+            root_data_path=args.data_path, name=args.trajectory
+        )
+        domain = traj_defn.domain
+        t_min = traj_defn.origin.datetime - traj_defn.duration.backward
+        t_max = traj_defn.origin.datetime + traj_defn.duration.forward
+    else:
+        raise NotImplementedError(timedomain_lookup)
+
+    def attempt_download():
+        download_named_domain(
+            data_path=args.data_path,
+            name=domain,
+            start_date=t_min,
+            end_date=t_max,
+            overwrite_existing=args.l_overwrite,
+        )
+
+    if args.retry_rate is not None:
+        while True:
+            attempt_download()
+            if download_complete(args.data_path, domain_name=domain):
+                break
+            else:
+                print(f"Sleeping {args.retry_rate}min...")
+                sleep(args.retry_rate * 60.0)
+                print("Retrying download")
+    else:
+        attempt_download()
+
+
+if __name__ == "__main__":
+    _run_cli(timedomain_lookup="by_arguments")
--- lagtraj/domain/download_by_trajectory.py
+++ lagtraj/domain/download_by_trajectory.py
@@ -1,35 +1,5 @@
-from pathlib import Path
-
-
-from .. import DEFAULT_ROOT_DATA_PATH
-from .download import download_named_domain
-from ..trajectory.load import load_definition as load_traj_definition
+from .download import _run_cli
 
 
 if __name__ == "__main__":
-    import argparse
-
-    argparser = argparse.ArgumentParser()
-    argparser.add_argument("trajectory")
-    argparser.add_argument(
-        "-d", "--data-path", default=DEFAULT_ROOT_DATA_PATH, type=Path
-    )
-    argparser.add_argument(
-        "-o", "--overwrite", dest="l_overwrite", action="store_true", default=False
-    )
-    args = argparser.parse_args()
-
-    traj_defn = load_traj_definition(
-        root_data_path=args.data_path, name=args.trajectory
-    )
-
-    t_min = traj_defn.origin.datetime - traj_defn.duration.backward
-    t_max = traj_defn.origin.datetime + traj_defn.duration.forward
-
-    download_named_domain(
-        data_path=args.data_path,
-        name=traj_defn.domain,
-        start_date=t_min,
-        end_date=t_max,
-        overwrite_existing=args.l_overwrite,
-    )
+    _run_cli(timedomain_lookup="by_trajectory")

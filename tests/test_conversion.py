d023da554b501c92c03dd01fdb688216a94f0a8d
Leif Denby <leif@denby.eu>
2020-11-06 12:33:59 +0000

    remove incomplete conversion test

--- tests/test_conversion.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-from lagtraj.conversion import build_conversion_data_path
-
-
-def test_convert_forcing_to_pkt_format(ds_forcing_linear_trajectory):
-    ds_forcing = ds_forcing_linear_trajectory
-    pass

32c7ce7a64ec1597784edc3df155e90fedd17e19
Leif Denby <leif@denby.eu>
2020-11-26 11:04:59 +0100

    Pre v0.1.0 cleanup (#88)
    * remove _eul in eurec4a circle domain name
    * remove unused files
    * minor refactor for distance calcs, move into module with geometry and remove duplicate module

--- lagtraj/utils/geometry.py
+++ lagtraj/utils/geometry.py
@@ -1,3 +1,20 @@
+import numpy as np
+from .constants import r_earth
+from scipy.constants import pi
+
+
+def lat_dist(lat1, lat2):
+    """Distance for difference in latitude only, at constant longitude"""
+    return np.deg2rad(lat2 - lat1) * r_earth
+
+
+def lon_dist(lon1, lon2, lat):
+    """Distance for difference in longitude, for given latitude"""
+    dlon_rad = np.deg2rad(lon2 - lon1)
+    dlon_rad = (dlon_rad + pi) % (2 * pi) - pi
+    return np.cos(np.deg2rad(lat)) * dlon_rad * r_earth
+
+
 def longitude_set_meridian(longitude):
     """Sets longitude to be between -180 and 180 degrees"""
     return (longitude + 180.0) % 360.0 - 180.0

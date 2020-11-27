2c0d5ee2639cd33fa19ccb6c2791a281433b44f1
Leif Denby <leif@denby.eu>
2020-11-27 13:50:18 +0100

    apply black

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,16 +4,13 @@ from lagtraj.utils.xarray import create_attributes_dictionary
 def test_create_output_attributes():
     attrs_dict = dict(
         name="a kind of input",
-        a_required_input=dict(
-            name="name of required_input",
-            param=42.0
-        )
+        a_required_input=dict(name="name of required_input", param=42.0),
     )
 
     expected_attrs = dict(
-        name=attrs_dict['name'],
-        a_required_input_name=attrs_dict['a_required_input']['name'],
-        a_required_input_param=attrs_dict['a_required_input']['param'],
+        name=attrs_dict["name"],
+        a_required_input_name=attrs_dict["a_required_input"]["name"],
+        a_required_input_param=attrs_dict["a_required_input"]["param"],
     )
 
     actual_attrs = create_attributes_dictionary(attrs_dict)

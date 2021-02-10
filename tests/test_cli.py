30d5513c4bd9993ced7abbfe45f868dbea9cf3ab
Leif Denby <leif@denby.eu>
2021-02-10 11:24:40 +0000

    Mock data download during CI CLI tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,10 +1,17 @@
+from unittest.mock import patch
+
 import lagtraj.forcings.create
 import lagtraj.trajectory.create
 
 from test_forcing_profiles_extraction import AVAILABLE_CONVERSIONS
 
 
-def test_cli(testdata_info):
+@patch("lagtraj.domain.download.download_complete")
+def test_cli(download_complete_mocked, testdata_info):
+    # during CI we won't have access to an ERA5 token so we need to pretend
+    # that all data has been downloaded
+    download_complete_mocked.return_value = True
+
     args = [
         testdata_info["trajectory_name"],
         "--data-path",

3f61135326729d2c1cafce7440eaba4eaad65451
Steven Boeing <stevenboeing@localhost.localdomain>
2021-02-19 09:45:47 +0000

    Weighted gradient calculations

--- lagtraj/utils/gradient_calculation.py
+++ lagtraj/utils/gradient_calculation.py
@@ -15,11 +15,13 @@ def _boundary_gradients(x_array, y_array, val_array):
     len_lons = np.shape(val_array)[3]
     x_gradient_array = np.empty((len_temp, len_levels))
     y_gradient_array = np.empty((len_temp, len_levels))
-    dval_dx = np.empty((len_lats))
-    dval_dy = np.empty((len_lons))
     for this_time in range(len_temp):
         for this_level in range(len_levels):
-            # This calculates the x-gradient at each latitude
+            dxval_tot = 0.0
+            dyval_tot = 0.0
+            dx_tot = 0.0
+            dy_tot = 0.0
+            # This calculates the x-gradient as a weighted average over latitude
             # The ~np.isnan operation is used to filter out masked values
             # The first and last filtered values are used
             for this_lat in range(len_lats):
@@ -28,32 +30,24 @@ def _boundary_gradients(x_array, y_array, val_array):
                 vals_filtered = vals_at_lat[~np.isnan(vals_at_lat)]
                 if len(vals_filtered) > 1:
                     x_filtered = x_at_lat[~np.isnan(vals_at_lat)]
-                    dvals = vals_filtered[-1] - vals_filtered[0]
-                    dval_dx[this_lat] = dvals / (x_filtered[-1] - x_filtered[0])
-                else:
-                    dval_dx[this_lat] = np.nan
-            # This similarly calculates the y-gradient at each longitude
+                    dxval_tot = dxval_tot + vals_filtered[-1] - vals_filtered[0]
+                    dx_tot = dx_tot + x_filtered[-1] - x_filtered[0]
+            # This similarly calculates the y-gradient weighted average
             for this_lon in range(len_lons):
                 vals_at_lon = val_array[this_time, this_level, :, this_lon].flatten()
                 y_at_lat = y_array[:, this_lon].flatten()
                 vals_filtered = vals_at_lon[~np.isnan(vals_at_lon)]
                 if len(vals_filtered) > 1:
                     y_filtered = y_at_lat[~np.isnan(vals_at_lon)]
-                    dvals = vals_filtered[-1] - vals_filtered[0]
-                    dval_dy[this_lon] = dvals / (y_filtered[-1] - y_filtered[0])
-                else:
-                    dval_dy[this_lon] = np.nan
+                    dyval_tot = dyval_tot + vals_filtered[-1] - vals_filtered[0]
+                    dy_tot = dy_tot + y_filtered[-1] - y_filtered[0]
             # Average these gradients (not weighted at this point, but filtering out all nan values due to e.g. division by zero!)
-            if len(~np.isnan(dval_dx)) > 0:
-                x_gradient_array[this_time, this_level] = np.mean(
-                    dval_dx[~np.isnan(dval_dx)]
-                )
+            if abs(dx_tot) > 1e-4:
+                x_gradient_array[this_time, this_level] = dxval_tot / dx_tot
             else:
                 x_gradient_array[this_time, this_level] = np.nan
-            if len(~np.isnan(dval_dy)) > 0:
-                y_gradient_array[this_time, this_level] = np.mean(
-                    dval_dy[~np.isnan(dval_dy)]
-                )
+            if abs(dy_tot) > 1e-4:
+                y_gradient_array[this_time, this_level] = dyval_tot / dy_tot
             else:
                 y_gradient_array[this_time, this_level] = np.nan
     return x_gradient_array, y_gradient_array

6572751a85981616e93fda264341dc79299cd1c0
Leif Denby <leif@denby.eu>
2022-01-10 10:47:58 +0000

    Improve era5 download completion check (#146)
    The era5 download complete check
    (`lagtraj.doamin.download.download_complete`) now checks for any missing
    files for a date range. Previously, we check if the CDSAPI backend had
    data-requests still processing and whether all files requested had been
    downloaded (based on the stored list of submitted data requests). This
    was unsufficient for checking download completion if the user changes
    the date range of their query.
    
    Also, rename all `t_start` -> `start_date` and `t_end` -> `end_date` and
    warn user if `datetime.datetime` object is passed in instead of
    `datetime.date` object. We previously ignored the time component anyway
    (because we fetch files by date) and `start_date` reflects this better
    than `t_start`.
    
    add --dry-run option to download command

--- lagtraj/domain/sources/era5/__init__.py
+++ lagtraj/domain/sources/era5/__init__.py
@@ -3,5 +3,5 @@ FILENAME_FORMAT = "{model_run_type}_{level_type}_{date}.nc"
 VERSION_FILENAME = "VERSION"
 
 
-from .download import download_data, all_data_is_downloaded  # noqa
+from .download import download_data, find_missing_files  # noqa
 from .load import load_data  # noqa

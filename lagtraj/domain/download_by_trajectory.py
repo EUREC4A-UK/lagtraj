c9f48c00d79a2c5d901d36a7815f0afad5239457
Leif Denby <leif@denby.eu>
2022-03-16 12:31:46 +0000

    expose download_by_trajectory cli for testing

--- lagtraj/domain/download_by_trajectory.py
+++ lagtraj/domain/download_by_trajectory.py
@@ -1,5 +1,9 @@
 from .download import _run_cli
 
 
+def cli(args):
+    _run_cli(args=args, timedomain_lookup="by_trajectory")
+
+
 if __name__ == "__main__":
-    _run_cli(timedomain_lookup="by_trajectory")
+    cli(args=None)
